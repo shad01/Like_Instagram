2c33fd393cd4f2f508c8db4b64bc0e85d6cd65f8
shad01 <antoniovictor256@gmail.com>
2018-12-13 22:27:08 -0200

    Bot Like Instagram

--- BotLike.py
+++ BotLike.py
@@ -1,7 +1,6 @@
 from time import sleep
 from selenium import webdriver
 from selenium.common.exceptions import ElementClickInterceptedException
-import pyautogui
 
 
 class Instagram(object):
@@ -12,44 +11,43 @@ class Instagram(object):
         self.browser = webdriver.Firefox()
         
     def login(self):
-        self.browser.get("https://www.instagram.com/")
+        self.browser.get('https://www.instagram.com/')
         sleep(2)
-        connect = self.browser.find_element_by_link_text("Conecte-se").click()
+        connect = self.browser.find_element_by_link_text('Conecte-se').click()
         sleep(3)
-        user = self.browser.find_element_by_name("username")
+        user = self.browser.find_element_by_name('username')
         user.send_keys(self.email)
-        passw = self.browser.find_element_by_name("password")
+        passw = self.browser.find_element_by_name('password')
         passw.send_keys(self.password)
         passw.submit()
     
     def like(self):
+        #procura pelo 'explore'
         sleep(7)
-        #procura pelo o 'explore'
         explore = self.browser.find_element_by_class_name(
-            """glyphsSpriteSafari__outline__24__grey_9.u-__7""")
+            '''glyphsSpriteSafari__outline__24__grey_9.u-__7''')
         #clica no 'explore'
         explore.click()
+        #procura pela img
         sleep(3)
-        #procura pela imagem
-        hrefs = self.browser.find_elements_by_tag_name("a")
-        href = [h.get_attribute("href") for h in hrefs]
+        a = self.browser.find_elements_by_tag_name('a')
+        href = [h.get_attribute('href') for h in a]
         imgs = [img for img in href if img[26:30] == 'p/Br']
-        print(len(imgs))
-        for a in imgs:
+        for i in imgs:
             try:
                 #abri a imagem
                 self.browser.get(a)
                 #botão curtir
                 dá_like = self.browser.find_element_by_class_name(
-                    """dCJp8.afkep.coreSpriteHeartOpen._0mzm-""")
+                    '''dCJp8.afkep.coreSpriteHeartOpen._0mzm-''')
                 #curti a imagem
                 dá_like.click()
             except ElementClickInterceptedException:
                 print("Falhou")
 
 
-user = str(input("Email: "))
-senha = str(input("Senha: "))
+user = str(input('Email: '))
+senha = str(input('Senha: '))
 insta = Instagram(user, senha)
 insta.login() 
 insta.like()
